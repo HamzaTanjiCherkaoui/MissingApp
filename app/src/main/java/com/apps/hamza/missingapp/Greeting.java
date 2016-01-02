2b46d77865ae3d1bd5d9cfe33b4a86e052dcbd44
hamza tanji cherkaoui <hoxa1235@gmail.com>
2016-01-02 15:24:16 +0000

    init github repo

---
+++ app/src/main/java/com/apps/hamza/missingapp/Greeting.java
@@ -0,0 +1,19 @@
+package com.apps.hamza.missingapp;
+
+/**
+ * Created by Asus on 02/01/2016.
+ */
+public class Greeting {
+
+    private String id;
+    private String content;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+}
---
+++ app/src/main/java/com/apps/hamza/missingapp/MissingPerson.java
@@ -0,0 +1,45 @@
+package com.apps.hamza.missingapp;
+
+/**
+ * Created by Asus on 02/01/2016.
+ */
+
+    public class MissingPerson {
+
+        private String firstName;
+        private String lastName;
+        private boolean status;
+
+
+        public MissingPerson() {
+
+        }
+
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+
+
+        public boolean isStatus() {
+            return status;
+        }
+
+        public void setStatus(boolean status) {
+            this.status = status;
+        }
+
+
+    }
